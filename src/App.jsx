48b0d65a4f189dba075e379587307cb49f85fe72
Osmar Andres Debegnach <andresdebegnach@gmail.com>
2021-11-26 03:17:55 -0300

    Se completo el prototipo del componente
    Se desarrollo una versión prototipo del componente MenuToggleButton

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MenuToggleButton from "./components/styled/MenuToggleButton";
+
+const App = () => {
+	return (
+		<div className="App">
+			<h1>Pruebas de Componentes</h1>
+			<MenuToggleButton color="#345" size="100px" />
+		</div>
+	);
+};
+
+export default App;
